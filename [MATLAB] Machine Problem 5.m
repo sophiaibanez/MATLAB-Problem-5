4db9ea71f4ece3cdb210e8f10e659dddc2de998d
sophiaibanez <57376444+sophiaibanez@users.noreply.github.com>
2019-12-08 03:20:26 +0800

    Update [MATLAB] Machine Problem 5.m

--- [MATLAB] Machine Problem 5.m
+++ [MATLAB] Machine Problem 5.m
@@ -1,6 +1,6 @@
 n = 0:199;
 x = input('Enter x(n) expression: ');
-plot(x)
+plot(x, 'LineWidth', 2)
 hold on
 
 for n = 1:length(x)
@@ -12,6 +12,7 @@ for n = 1:length(x)
         y(n) = 1.5*x(n) - 2*x(n-1) + 0.5*x(n-2);
     end
 end
-stem(y(n))
+stem(y(n), '-r', 'LineWidth', 2)
+legend('x(n)','y(n)')    
     
     

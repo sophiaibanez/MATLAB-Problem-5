3ba8afb5fc9314cc101b3696a1368f35d9c59b43
sophiaibanez <57376444+sophiaibanez@users.noreply.github.com>
2019-12-08 13:37:29 +0800

    Update [MATLAB] Machine Problem 5.m

--- [MATLAB] Machine Problem 5.m
+++ [MATLAB] Machine Problem 5.m
@@ -1,21 +1,19 @@
-n = 0:199;
+n = 0:200;
 x = input('Enter x(n) expression: ');
 y = zeros(size(x));
 
-for i = 1:200
+for i = 1:201
   if i == 0
-    x(i)= x(i+2);
-    y(i) = -1.5*x(i) + 2*x(i+1) - 0.5*x(i+2);
+    y(i) = -1.5*x(i+2) + 2*x(i+3) - 0.5*x(i+4);
   elseif i > 0 && i <= 198
-    x(i)= x(i+2);
-    y(i) = 0.5*x(i+1) - 0.5*x(i-1);
+    y(i) = 0.5*x(i+3) - 0.5*x(i+1);
   elseif i == 199
-    x(i)= x(i+2);
-    y(i) = 1.5*x(i) - 2*x(i-1) + 0.5*x(i-2);
+    y(i) = 1.5*x(i+2) - 2*x(i+1) + 0.5*x(i);
   end
 end
 plot(x, 'LineWidth', 2)
 hold on
 plot(y, '-r', 'LineWidth', 2)
+title('Graphs: x(n) and y(n)')
 legend('x(n)','y(n)')    
-axis tight    
+axis tight  
